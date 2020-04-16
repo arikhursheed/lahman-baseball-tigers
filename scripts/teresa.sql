84b54df07e21f479c594a74ec9a7306afe525660
Teresa <analytics@Teresas-MacBook-Pro.local>
2020-04-16 16:44:09 -0500

    start on q9

--- scripts/teresa.sql
+++ scripts/teresa.sql
@@ -122,3 +122,13 @@ group by fullname
 order by total_salary desc
 */
 
+/*
+9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
+Give their full name and the teams that they were managing when they won the award.
+*/
+
+--list of managers and year they won specified award, still working on structuring the join to managers and people tables to get full name and team
+select playerid, yearid
+from awardsmanagers
+where awardid like 'TSN%'
+and (lgid like 'NL' or lgid like 'AL');
