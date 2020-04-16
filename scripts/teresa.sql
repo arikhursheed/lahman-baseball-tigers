8130a46852166ce6eafd742d99bb4d3d6338c1f7
Teresa <analytics@Teresas-MacBook-Pro.local>
2020-04-16 15:34:30 -0500

    helped on q3

--- scripts/teresa.sql
+++ scripts/teresa.sql
@@ -95,3 +95,30 @@ and games > 10
 order by avg_attendance
 limit 5;
 */
+
+/*
+3. Find all players in the database who played at Vanderbilt University. 
+Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
+Sort this list in descending order by the total salary earned. 
+Which Vanderbilt player earned the most money in the majors?
+*/
+
+/*
+select *
+from collegeplaying
+where schoolid like 'vand%'
+
+select *
+from salaries
+
+select concat(namefirst,' ', namelast) as fullname, sum(salary) as total_salary
+from salaries
+	inner join collegeplaying
+	using (playerid)
+	inner join people
+	using (playerid)
+where schoolid ilike 'vand%'
+group by fullname
+order by total_salary desc
+*/
+
