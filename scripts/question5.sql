2ab0b56ac76b19a9071f1793df802f5114b6fd57
nmuldowney <muldowneynicole@gmail.com>
2020-04-17 11:52:08 -0500

    final question 5

--- scripts/question5.sql
+++ scripts/question5.sql
@@ -1,20 +1,23 @@
---Q5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. 
---Do the same for home runs per game. Do you see any trends?
+/*
+Q5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. 
+Do the same for home runs per game. Do you see any trends? 
+*/
+--The avg_hr and avg_so per game trend in the same direction for the most part.
 	 
 WITH so_pergame_year AS (SELECT yearid, SUM(so)::numeric/(SUM(g)/2)::numeric AS so_pergame 
 						  FROM teams GROUP BY yearid),
     hr_pergame_year AS (SELECT yearid, SUM(hr)::numeric/(SUM(g)/2)::numeric AS hr_pergame 
-					  	FROM teams GROUP BY yearid)
+					  	  FROM teams GROUP BY yearid)
 SELECT CASE WHEN yearid BETWEEN '1920' AND '1929' THEN '1920s'
-	        WHEN yearid between '1930' and '1939' then '1930s'
-			WHEN yearid between '1940' and '1949' then '1940s'
-			WHEN yearid between '1950' and '1959' then '1950s'
-			WHEN yearid between '1960' and '1969' then '1960s'
-			WHEN yearid between '1970' and '1979' then '1970s'
-			WHEN yearid between '1980' and '1989' then '1980s'
-			WHEN yearid between '1990' and '1999' then '1990s'
-			WHEN yearid between '2000' and '2009' then '2000s'
-			WHEN yearid between '2010' and '2019' then '2010s' ELSE 'notcounted' END AS decade,
+	        WHEN yearid BETWEEN '1930' AND '1939' THEN '1930s'
+			WHEN yearid BETWEEN '1940' AND '1949' THEN '1940s'
+			WHEN yearid BETWEEN '1950' AND '1959' THEN '1950s'
+			WHEN yearid BETWEEN '1960' AND '1969' THEN '1960s'
+			WHEN yearid BETWEEN '1970' AND '1979' THEN '1970s'
+			WHEN yearid BETWEEN '1980' AND '1989' THEN '1980s'
+			WHEN yearid BETWEEN '1990' AND '1999' THEN '1990s'
+			WHEN yearid BETWEEN '2000' AND '2009' THEN '2000s'
+			WHEN yearid BETWEEN '2010' AND '2019' THEN '2010s' END AS decade,
 			ROUND(AVG(hr_pergame), 2) AS avg_hr_pergame,
 			ROUND(AVG(so_pergame), 2) AS avg_so_pergame
 FROM teams
